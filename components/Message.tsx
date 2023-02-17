35a4176f620431471a0ae3e272a9073e3960f15b
ShrutiSemwal <semwalshruti450@gmail.com>
2023-02-17 12:01:41 +0530

    Few changes done

--- components/Message.tsx
+++ components/Message.tsx
@@ -8,7 +8,7 @@ function Message({message} : Props) {
     const isChatGPT = message.user.name === "ChatGPT";
   return (
     <div className={`py-5 text-white ${isChatGPT && "bg-[#434654]"}`}>
-        <div className="flex space-x-5 px-10 max-auto ">
+        <div className="flex space-x-5 px-10 max-w-2xl max-auto ">
             <img src={message.user.avatar} 
             alt=""
             className="h-8 w-8"
